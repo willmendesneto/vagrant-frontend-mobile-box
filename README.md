f0b4500f2b3a6791d22dda6184ce90ef047bce76
willmendesneto <willmendesneto@gmail.com>
2015-02-14 19:58:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+Vagrant NG Box
+=============================
+
+
+Vagrant NG Box is a ready-to-go hybrid development environment for building mobile apps with Ionic, Cordova, and Android based in Ionic Box, with some modifications. Vagrant NG Box was built to make it easier for developers to build Android versions of their app, and especially for Windows users to get a complete dev environment set up without all the headaches.
+
+For iOS developers, Vagrant NG Box won't do much for you right now unless you are having trouble installing the Android SDK, and Vagrant NG Box cannot be used for iOS development for a variety of legal reasons (however, the `ionic package` command in beta will soon fix that).
+
+
+### Default packages installed
+
+Packages installeds by default:
+
+- Git;
+- Java;
+- "PT_BR" Package Language;
+- Android SDK;
+- Sass;
+- Compass;
+
+
+#### CLI
+
+Was installed some functions and aliases for use in this box. The files `.functions` and `.aliases` are installed in `vagrant` user root folder and loaded in user login.
+
+
+#### NodeJS
+
+This vagrant box is using NVM (https://raw.github.com/creationix/nvm) with setup:
+
+- NodeJS (Version 0.10.33);
+- NPM (Version 2.2.0);
+- cordova;
+- ionic;
+- yeoman;
+- bower;
+- grunt-cli;
+- gulp;
+- jshint;
+- protractor;
+- karma;
+
+
+### Provision
+
+This box is provisioned via shell script. If you would like to modify the package for your use, please take a look in [bootstrap.sh file](bootstrap.sh file)
+
+
+### Installation
+
+To install, download and install [Vagrant](https://www.vagrantup.com/downloads.html) for your platform, then download and install [VirtualBox](http://virtualbox.org/).
+
+Once Vagrant and VirtualBox are installed, you can download the latest release of this GitHub repo, and unzip it. `cd` into the unzipped folder and run:
+
+```bash
+$ vagrant up
+$ vagrant ssh
+```
+
+The username for vagrant is `vagrant` and the password is `vagrant`.
+
+This will download and install the image, and then go through the dependencies and install them one by one. `vagrant ssh` will connect you to the image and give you a bash prompt. Once everything completes, you'll have a working box to build your apps on Android.
+
+### Connected Android Devices
+
+The image also has support for connected USB Android devices. To test whether devices are connected, you can run (from the box):
+
+```bash
+$ sudo /home/vagrant/android-sdk-linux/platform-tools/adb devices
+```
+
+If that does not work, or shows `????? permissions`, then run:
+
+```bash
+sudo /home/vagrant/android-sdk-linux/platform-tools/adb kill-server
+sudo /home/vagrant/android-sdk-linux/platform-tools/adb start-server
+```
---
+++ bootstrap.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+ANDROID_SDK_FILENAME=android-sdk_r23.0.2-linux.tgz
+ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
+
+#sudo apt-get install python-software-properties
+#sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update -y
+sudo apt-get install -y curl git git-core
+sudo apt-get install -y git openjdk-7-jdk ant expect
+#ADB Not Responding
+sudo apt-get install -y lib32z1 lib32z1-dev lib32stdc++6
+#apt-get install ruby-full
+sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
+
+# Install pt_BR
+sudo locale-gen pt_BR.UTF-8
+
+curl -O $ANDROID_SDK
+tar -xzvf $ANDROID_SDK_FILENAME
+sudo chown -R vagrant android-sdk-linux/
+sudo rm -rf $ANDROID_SDK_FILENAME
+
+# Install NVM
+cd /home/vagrant
+wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Install NodeJS
+nvm install 0.10.33
+nvm use 0.10.33
+nvm alias default 0.10.33
+
+# Update NPM
+# npm update -g npm@latest
+npm update -g npm@2.2.0
+
+# Install Node Packages
+npm install -g cordova
+npm install -g ionic
+npm install -g yo
+npm install -g bower
+npm install -g grunt-cli
+npm install -g jshint
+npm install -g protractor
+npm install -g gulp
+npm install -g karma
+
+# Install Yeoman Generator
+
+
+# Add .function and .aliases files in box
+curl -L http://goo.gl/2e3iWT > .functions
+curl -L http://goo.gl/ii1v3n > .aliases
+
+# Add new contents in .bashrc
+echo "" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo " # Source global definitions" >> ~/.bashrc
+echo "if [ -f ~/.functions ]; then" >> ~/.bashrc
+echo "  . ~/.functions" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo " # Source global definitions" >> ~/.bashrc
+echo "if [ -f ~/.aliases ]; then" >> ~/.bashrc
+echo "  . ~/.aliases" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "export NVM_DIR=\"\$HOME/.nvm\"" >> ~/.bashrc
+echo "#Add new values from variable environment" >> ~/.bashrc
+echo "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386/jre/bin/java" >> ~/.bashrc
+echo "ANDROID_HOME=~/android-sdk-linux" >> ~/.bashrc
+echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:\$NVM_DIR/bin" >> ~/.bashrc
+# This loads nvm
+echo "[[ -s \"\$NVM_DIR/nvm.sh\" ]] && . \"\$NVM_DIR/nvm.sh\"" >> ~/.bashrc
+
+source ~/.bashrc
+
+
+# Installs RubyGems
+sudo gem update --system #gets the latest version of the RubyGems installer
+sudo gem install rubygems-update
+sudo update_rubygems
+sudo gem install sass
+sudo apt-get install -y ruby-compass
+
+expect -c '
+set timeout -1   ;
+spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-19,build-tools-19.1.0
+expect {
+    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+    eof
+}
+'
+# alias for adb
+echo "alias adb=\"/home/vagrant/android-sdk-linux/platform-tools/adb\"" >> /home/vagrant/.bashrc
+
+sudo /home/vagrant/android-sdk-linux/platform-tools/adb kill-server
+sudo /home/vagrant/android-sdk-linux/platform-tools/adb start-server
+sudo /home/vagrant/android-sdk-linux/platform-tools/adb devices
