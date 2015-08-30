bd6f07da8f1f6a1c588d570a477b017136332e99
Will Mendes <willmendesneto@gmail.com>
2015-08-30 19:00:27 -0300

    Update box dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,42 @@
 #!/usr/bin/env bash
 
+echo ""
+echo "        _    _ _____ _     _____ ________  ___ _____ "
+echo "       | |  | |  ___| |   /  __ \  _  |  \/  ||  ___|"
+echo "       | |/\| |  __|| |   | |   | | | | |\/| ||  __| "
+echo "       \  /\  / |___| |___| \__/\ \_/ / |  | || |___ "
+echo "        \/  \/\____/\_____/\____/\___/\_|  |_/\____/ "
+echo ""
+echo ""
+echo ""
+echo "                                                               "
+echo "                        ,-----.                                "
+echo "                      ,'_/_|_\_\`.                             "
+echo "                     /<<::8[O]::>\                             "
+echo "                    _|-----------|_                            "
+echo "                :::|  | ====-=- |  |:::                        "
+echo "                :::|  | -=-==== |  |:::                        "
+echo "                :::\  | ::::|()||  /:::                        "
+echo "                ::::| | ....|()|| |::::                        "
+echo "                    | |_________| |                            "
+echo "                    | |\_______/| |                            "
+echo "                   /   \ /   \ /   \                           "
+echo "                   \`---' \`---' \`---'                        "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ">"
+echo ">>"
+echo ">>> Downloading Android SDK..."
+
 # Configuration Variables
-ANDROID_SDK_FILENAME=android-sdk_r23.0.2-linux.tgz
+ANDROID_SDK_FILENAME=android-sdk_r24-linux.tgz
 ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
+
+echo ""
+echo ">>>  Android SDK: ✔"
+echo ""
 
 # This boolean is used for load bash or zsh in your CLI environment
 # By default is loaded .bashrc, if you would like to use .zsh + oh-my-zsh in your CLI
@@ -17,16 +51,35 @@ sudo apt-get install -y openjdk-7-jdk ant expect
 #ADB Not Responding
 sudo apt-get install -y lib32z1 lib32z1-dev lib32stdc++6 tmux
 #apt-get install ruby-full
-sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
+sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev
+sudo apt-get install -y zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
+sudo apt-get install -y libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
+sudo apt-get install -y automake libtool bison subversion pkg-config sqlite3
 
+
+echo ""
+echo ">>>  Android SDK: ✔"
+echo ""
 
 # Install pt_BR
-sudo locale-gen pt_BR.UTF-8
+# sudo locale-gen pt_BR.UTF-8
+
+echo ""
+echo ">>>  Unzipping Android SDK..."
+echo ""
 
 curl -O $ANDROID_SDK
 tar -xzvf $ANDROID_SDK_FILENAME
 sudo chown -R vagrant android-sdk-linux/
 sudo rm -rf $ANDROID_SDK_FILENAME
+
+echo ""
+echo ">>>  Unzipped Android SDK: ✔"
+echo ""
+
+echo ""
+echo ">>> Installing NodeJS..."
+echo ""
 
 # Install NVM
 cd /home/vagrant
@@ -35,13 +88,13 @@ export NVM_DIR="/home/vagrant/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # Install NodeJS
-nvm install 0.10.33
-nvm use 0.10.33
-nvm alias default 0.10.33
+nvm install 0.12.5
+nvm use 0.12.5
+nvm alias default 0.12.5
 
 # Update NPM
 # npm install -g npm@latest
-npm install -g npm@2.2.0
+npm install -g npm@2.11.2
 
 # Install Node Packages
 npm install -g cordova
@@ -57,6 +110,15 @@ npm install -g trash
 
 # Install Yeoman Generator
 
+
+echo ""
+echo ">>> NodeJS: ✔"
+echo ""
+
+
+echo ""
+echo ">>> CLI Configurations..."
+echo ""
 
 # Add .function, .aliases and Z files in box
 curl -L http://goo.gl/2e3iWT > .functions
@@ -104,6 +166,15 @@ echo "[[ -s \"\$NVM_DIR/nvm.sh\" ]] && . \"\$NVM_DIR/nvm.sh\"" >> $CLI_LOAD_FILE
 source $CLI_LOAD_FILE
 
 
+echo ""
+echo ">>> CLI Configurations: ✔"
+echo ""
+
+
+echo ""
+echo ">>> Update Ruby in Box..."
+echo ""
+
 # Installs RubyGems
 sudo gem update --system #gets the latest version of the RubyGems installer
 sudo gem install rubygems-update
@@ -111,10 +182,19 @@ sudo update_rubygems
 sudo gem install sass
 sudo apt-get install -y ruby-compass
 
+echo ""
+echo ">>> Ruby: ✔"
+echo ""
+
+
 # Install code for course
 # If you need, please remove the coments
 # cd /vagrant_data && git clone https://github.com/willmendesneto/angularjs-course.git
 
+
+echo ""
+echo ">>> Update Android SDK..."
+echo ""
 
 expect -c '
 set timeout -1   ;
@@ -124,6 +204,7 @@ expect {
     eof
 }
 '
+
 # alias for adb
 echo "alias adb=\"/home/vagrant/android-sdk-linux/platform-tools/adb\"" >> $CLI_LOAD_FILE
 echo "alias vd=\"cd /vagrant_data\"" >> $CLI_LOAD_FILE
@@ -133,3 +214,41 @@ sudo /home/vagrant/android-sdk-linux/platform-tools/adb start-server
 sudo /home/vagrant/android-sdk-linux/platform-tools/adb devices
 
 
+echo ""
+echo ">>> Update Android SDK: ✔"
+echo ""
+echo ""
+echo ""
+echo ">>> STATUS: ✔"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "          _.-'~~~~~~\`-._            "
+echo "         /      ||      \            "
+echo "        /       ||       \           "
+echo "       |        ||        |          "
+echo "       | _______||_______ |          "
+echo "       |/ ----- \/ ----- \|          "
+echo "      /  (     )  (     )  \         "
+echo "     / \  ----- () -----  / \        "
+echo "    /   \      /||\      /   \       "
+echo "   /     \    /||||\    /     \      "
+echo "  /       \  /||||||\  /       \     "
+echo " /_        \o========o/        _\    "
+echo "   \`--...__|\`-._  _.-'|__...--'    "
+echo "           |    \`'    |             "
+echo ""
+echo ""
+echo ""
+echo "ALIAS:"
+echo ""
+echo "adb: alias for \"/home/vagrant/android-sdk-linux/platform-tools/adb\""
+echo "vd: alias for \"cd /vagrant_data\""
+echo ""
+echo ""
+echo ">>> So, let's code!"
+echo ""
+echo "May the force be with you! lo/"
